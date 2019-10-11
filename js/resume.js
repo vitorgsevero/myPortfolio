5f75dd82aecfaeea284142522d5fe1a2e03b7ab2
vitorgsevero <vitorgsevero@gmail.com>
2019-10-11 12:13:02 -0300

    Fixing Dev Icons

--- js/resume.js
+++ js/resume.js
@@ -25,20 +25,6 @@
     target: '#sideNav'
   });
 
-  // Portfolio isotope and filter
-var portfolioIsotope = $('.portfolio-container').isotope({
-  itemSelector: '.portfolio-item',
-  layoutMode: 'fitRows'
-});
-
-$('#portfolio-flters li').on( 'click', function() {
-  $("#portfolio-flters li").removeClass('filter-active');
-  $(this).addClass('filter-active');
-
-  portfolioIsotope.isotope({ filter: $(this).data('filter') });
-});
-
-
 })(jQuery); // End of use strict
 
 
